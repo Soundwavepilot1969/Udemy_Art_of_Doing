eb8a4c0d83ec2dee67a1c7a7b362ae6ef00b5241
Soundwavepilot1969 <ashwin.radhakrishnan3190@gmail.com>
2021-05-03 11:43:11 +0530

    First Commit

---
+++ App0_GUI_World/notes.py
@@ -0,0 +1,4 @@
+# In cmd line pythong to start typing and running python code directly in it
+# exit() for exiting python in cmd line
+# pip freeze to list out all installed packages
+# ctrl+shift+p in vscode brings up command palette. Select pythong interpreter from here

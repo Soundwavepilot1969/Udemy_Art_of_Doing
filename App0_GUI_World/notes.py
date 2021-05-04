ecc53441bba8d73270bcefb16da867e582288a8e
Soundwavepilot1969 <ashwin.radhakrishnan3190@gmail.com>
2021-05-04 09:44:47 +0530

    removed notes file from App0

--- App0_GUI_World/notes.py
+++
@@ -1,4 +0,0 @@
-# In cmd line pythong to start typing and running python code directly in it
-# exit() for exiting python in cmd line
-# pip freeze to list out all installed packages
-# ctrl+shift+p in vscode brings up command palette. Select pythong interpreter from here
